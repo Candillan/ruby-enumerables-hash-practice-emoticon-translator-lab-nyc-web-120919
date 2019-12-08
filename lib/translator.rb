794d41c7ceb38103948d48e986e8bbc04db72918
Calvin Baker <calvin.baker128@gmail.com>
2019-12-08 07:51:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ requires "yaml"
 
 def load_library
   # code goes here
-  emotes = YAML.load_file('emoticons.yml')
+  emotes = YAML.load_file(path)
   
   result = {
     "get_english_meaning" => {},
@@ -11,15 +11,26 @@ def load_library
   }
   
   emotes.each { |key, value|
-  
+	  result[“get_japanese_emoticon”][value[0]] = emotes[key[1]
+	  result[“get_english_meaning”][value[1]] = key
+	}
+  result
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(path, emotes)
   # code goes here
-  
+  result = load_library(path)[“get_japanese_emoticon”][emotes]
+  if !result
+    return “Sorry, that emoticon was not found”
+  end
+  result
 end
 
 def get_english_meaning
   # code goes here
+  result = load_library(path)[“get_english_meaning”][emotes]
+  if !result
+    return “Sorry, that emoticon was not found”
+  end
+  result
 end
-
